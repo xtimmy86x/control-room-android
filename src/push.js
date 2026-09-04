b4ad1aaee2b1a10b349c351a29021aff7ee53d6b
Alessandro Manighetti <76836856+xtimmy86x@users.noreply.github.com>
2026-09-04 18:44:02 +0200

    feat: report Android 0.3.0 to Control Room

--- src/push.js
+++ src/push.js
@@ -4,7 +4,7 @@ import { PushNotifications } from '@capacitor/push-notifications';
 import { request } from './api.js';
 import { secureStore } from './secure-store.js';
 
-const APP_VERSION = '0.2.6';
+const APP_VERSION = '0.3.0';
 let listenersInstalled = false;
 let pushStartedForToken = null;
 let registered = false;
